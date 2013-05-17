ab6ec2a8663d8b99b12dd70ce0401548025977fa
Martin Plöger <martin.ploeger@web.de>
2013-05-17 20:25:09 +0200

    added a little more structure

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,1 +1,13 @@
+echo "TODO sort files into folders"
+echo "TODO import ploeger zprezto theme"
 echo "TODO fill with backup of old .dotfiles and symlinking"
+# .gitconfig
+# .gitignore_global
+# .irbrc
+# .pryrc
+# .zlogin
+# .zlogout
+# .zpreztorc
+# .zprofile
+# .zshenv
+# .zshrc
